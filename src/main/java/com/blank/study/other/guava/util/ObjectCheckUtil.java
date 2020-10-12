c04e060e6adff0260082855f392f17ca91b41257
blank <1071453055@qq.com>
2020-10-12 15:52:53 +0800

    feat:增加Object检查工具类

---
+++ src/main/java/com/blank/study/other/guava/util/ObjectCheckUtil.java
@@ -0,0 +1,26 @@
+package com.blank.study.other.guava.util;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+/**
+ * Check Object Something
+ * @author wudc
+ * @date 2020/10/12 12:08
+ */
+public class ObjectCheckUtil {
+
+	/**
+	 * Check not null
+	 */
+	public static <T extends @NonNull Object> T checkNotNull(T object) {
+		if (object instanceof String) {
+			if (((String) object).isEmpty()) {
+				object = null;
+			}
+		}
+		if (object == null) {
+			throw new NullPointerException();
+		}
+		return object;
+	}
+}
